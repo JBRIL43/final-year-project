b9a0f901420e2495a239728d6395f06617ccf96b
JBRIL43 <jibrilnuredin@hu.edu.et>
2026-04-08 14:06:08 +0300

    Configure FlutterFire and generate Firebase options

--- frontend/mobile_app/lib/main.dart
+++ frontend/mobile_app/lib/main.dart
@@ -7,9 +7,7 @@ import 'screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
